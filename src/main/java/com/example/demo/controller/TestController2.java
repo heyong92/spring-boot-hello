a61b2979aeccba4519fab1b3ef7c2fa59f9c8618
heyong123 <779091752@qq.com>
2018-08-27 20:21:10 +0800

    第3次提交

--- src/main/java/com/example/demo/controller/TestController2.java
+++ src/main/java/com/example/demo/controller/TestController2.java
@@ -3,16 +3,11 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class TestController {
-@RequestMapping("/showStrq1")
+public class TestController2 {
+@RequestMapping("/showStr2111221")
     public String showStr(String str){
-
-        return str;
-    }
-
-
-    public String index(String str){
 
         return str;
     }
