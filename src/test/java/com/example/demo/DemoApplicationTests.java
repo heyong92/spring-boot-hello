fd6aae5e03b8929bd5c99e9947e100b6ab140df1
heyong123 <779091752@qq.com>
2018-09-24 21:33:25 +0800

    123t

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,9 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
 	@Test
-	public void contextLoadsMain() {
+	public void contextLoadsMain_t() {
 	}
 
 }
